ca0b1c4db8e9764fc505d33a90e652fd66a04ea3
Thomas Detemmerman <thomas.detemmerman@realdolmen.com>
2025-08-05 22:00:35 +0200

    trial attestation report

--- App/App.cpp
+++ App/App.cpp
@@ -16,6 +16,44 @@ void ocall_wait_keyinput(const char *str)
     getchar();
 }
 
+bool create_app_enclave_report(const char* enclave_path, sgx_target_info_t qe_target_info, sgx_report_t *app_report, const sgx_report_data_t* p_data)
+{
+    bool ret = true;
+    uint32_t retval = 0;
+    sgx_status_t sgx_status = SGX_SUCCESS;
+    sgx_enclave_id_t eid = 0;
+    int launch_token_updated = 0;
+    sgx_launch_token_t launch_token = { 0 };
+
+    sgx_status = sgx_create_enclave(enclave_path,
+                                    SGX_DEBUG_FLAG,
+                                    &launch_token,
+                                    &launch_token_updated,
+                                    &eid,
+                                    NULL);
+    if (SGX_SUCCESS != sgx_status) {
+        printf("Error, call sgx_create_enclave fail [%s], SGXError:%04x.\n", __FUNCTION__, sgx_status);
+        ret = false;
+        sgx_destroy_enclave(eid);
+        return ret;
+    }
+
+    sgx_status = enclave_create_report(eid,
+                                       &retval,
+                                       &qe_target_info,
+                                       p_data,
+				       app_report);
+    if ((SGX_SUCCESS != sgx_status) || (0 != retval)) {
+        printf("\nCall to get_app_enclave_report() failed\n");
+        ret = false;
+        sgx_destroy_enclave(eid);
+        return ret;
+    }
+
+    sgx_destroy_enclave(eid);
+    return ret;
+}
+
 /* application entry */
 int SGX_CDECL main(int argc, char *argv[])
 {
