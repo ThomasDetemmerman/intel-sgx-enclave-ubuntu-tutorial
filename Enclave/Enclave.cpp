43377393c319eaaf5953e7bb92b4607e269394af
Thomas Detemmerman <thomas.detemmerman@realdolmen.com>
2025-08-05 21:57:24 +0200

    trial attestation report

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -8,3 +8,17 @@ int trusted_func01()
     ocall_wait_keyinput("Please enter keyboard to show variables in memory ...");
     return trusted_x;
 }
+
+
+
+#include "sgx_trts.h"
+#include "sgx_error.h"
+#include "sgx_report.h"
+#include "sgx_utils.h"
+
+uint32_t enclave_create_report(const sgx_target_info_t* p_qe3_target,
+                const sgx_report_data_t* p_data,
+		sgx_report_t* p_report)
+{
+    return sgx_create_report(p_qe3_target, p_data, p_report);
+}
