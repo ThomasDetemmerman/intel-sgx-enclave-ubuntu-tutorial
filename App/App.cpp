37465813adf6a65dd4bb5c0fd6bb386a20b3726a
Thomas Detemmerman <thomas.detemmerman@realdolmen.com>
2025-08-05 22:47:23 +0200

    trial attestation report

--- App/App.cpp
+++ App/App.cpp
@@ -42,6 +42,36 @@ void ocall_wait_keyinput(const char *str)
     getchar();
 }
 
+const char *format_hex_buffer (char *buffer, uint maxSize, uint8_t *data, size_t size)
+{
+    if (size * 2 >= maxSize)
+        return "DEADBEEF";
+
+    for (int i=0; i < size; i++)
+    {
+        sprintf(&buffer[i*2], "%02X", data[i]);
+    }
+    buffer[size*2+1] = '\0';
+    return buffer;
+}
+
+
+const char *uint16_to_buffer (char *buffer, uint maxSize, uint16_t n, size_t size)
+{
+    if (size * 2 >= maxSize || size < 2)
+        return "DEADBEEF";
+    sprintf(&buffer[0], "%02X", uint8_t(n));
+    sprintf(&buffer[2], "%02X", uint8_t(n >> 8));
+
+    for (int i=2; i < size; i++)
+    {
+        sprintf(&buffer[i*2], "%02X", 0);
+    }
+    buffer[size*2+1] = '\0';
+    return buffer;
+}
+
+
 bool create_app_enclave_report(const char* enclave_path, sgx_target_info_t qe_target_info, sgx_report_t *app_report, const sgx_report_data_t* p_data)
 {
     bool ret = true;
