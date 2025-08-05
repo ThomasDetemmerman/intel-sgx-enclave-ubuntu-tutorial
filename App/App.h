e49a3983ed06f22884e6a02b29b6d4a1a60bc5ee
Thomas Detemmerman <thomas.detemmerman@realdolmen.com>
2025-08-05 22:12:36 +0200

    trial attestation report

--- App/App.h
+++ App/App.h
@@ -4,21 +4,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <string>
-#include <iostream>
-#include <iomanip>
-#include <sstream>
-#include <openssl/sha.h>
 
-#include "sgx_error.h"   /* sgx_status_t */
-#include "sgx_eid.h"     /* sgx_enclave_id_t */
-#include "sgx_urts.h"
-#include "sgx_quote_3.h"
-#include "sgx_dcap_ql_wrapper.h"
 #include "sgx_error.h"       /* sgx_status_t */
 #include "sgx_eid.h"     /* sgx_enclave_id_t */
 
