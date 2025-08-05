21345559b9de7133303c7b8ebcee9838d74d7ace
Thomas Detemmerman <thomas.detemmerman@realdolmen.com>
2025-08-05 19:48:41 +0200

    trial attestation report

--- App/App.cpp
+++ App/App.cpp
@@ -33,7 +33,40 @@ int SGX_CDECL main(int argc, char *argv[])
         getchar();
         return -1;
     }
- 
+    printf("Enclave initialized successfully. Starting attestation\n");
+    //perform attestation
+    // 1) Get report from enclave
+    sgx_report_t report;
+    ret = get_enclave_report(global_eid, &report); 
+    if (ret != SGX_SUCCESS) { 
+        printf("Error with getting attestation\n");
+        getchar();
+        return -1;
+    }
+    // 2) Get quote size
+    uint32_t quote_size = 0;
+    ret = sgx_get_quote_size(NULL, &quote_size);
+    if (ret != SGX_SUCCESS) { 
+        printf("Error getting the size of the quote\n");
+        getchar();
+        return -1;
+    }
+    // 3) Allocate buffer for quote
+    uint8_t *quote = malloc(quote_size);
+    if (!quote) { 
+        printf("Error allocating buffer for quote\n");
+        getchar();
+        return -1;
+    }
+    // 4) Get quote from report
+    ret = sgx_get_quote(&report, SGX_LINKABLE_SIGNATURE, NULL, 0, NULL, quote, quote_size);
+    if (ret != SGX_SUCCESS)  { 
+        printf("Error getting quote from report\n");
+        getchar();
+        return -1;
+    }
+    printf("Attestation successful. Quote size: %d\n", quote_size);
+    
     // invoke trusted_func01();
     int returned_result;
     ret = trusted_func01(global_eid, &returned_result);
--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -1,10 +1,23 @@
 #include "Enclave_t.h"
 #include <stdio.h>
 #include <string.h>
+#include <sgx_tcrypto.h>
+#include <sgx_trts.h>
+#include <sgx_tkey_exchange.h>
+#include <sgx_report.h>
 
 int trusted_func01()
 {
     int trusted_x = 987654321;
     ocall_wait_keyinput("Please enter keyboard to show variables in memory ...");
     return trusted_x;
+}
+
+sgx_status_t get_enclave_report(sgx_report_t *report)
+{
+    if (report == NULL)
+        return SGX_ERROR_INVALID_PARAMETER;
+
+    sgx_status_t status = sgx_create_report(NULL, NULL, report);
+    return status;
 }
